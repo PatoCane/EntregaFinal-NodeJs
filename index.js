cb48e2bbc52ceb7c51277176d37338a283064351
Patricia Alejandra Canestro <pato030668@gmail.com>
2025-12-05 20:05:51 -0300

    Fix: Configuración final para Vercel Serverless (index.js y vercel.json)

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 import express from "express"
 import cors from "cors"
-import { configDotenv } from "dotenv"
+
 import rutasLog from "./src/routes/auth.routes.js"
 import rutasProductos from "./src/routes/products.routes.js"
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 const corsConfig = {
-    origin: ['http://localhost:3000', 'https://midominio.com'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],                  
-    allowedHeaders: ['Content-Type', 'Authorization'],          
-    exposedHeaders: ['Content-Length'],                         
-    credentials: true,                                         
-    maxAge: 600,                                               
-    optionsSuccessStatus: 204                                  
+    // CAMBIAR A TU DOMINIO DE VERSEL AQUÍ
+    origin: ['http://localhost:3000', 'https://entrega-final-lac.vercel.app'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    exposedHeaders: ['Content-Length'], 
+    credentials: true, 
+    maxAge: 600, 
+    optionsSuccessStatus: 204 
 }
 
 app.use(cors(corsConfig))
-app.use(express.json())
+app.use(express.json()) // Línea única y correcta
 
 app.use("/api", rutasLog)
+app.use("/api", rutasProductos)
 
+// Manejadores de errores y 404
 app.use((req, res, next) => {
     console.log(`Datos received at:  ${req.method} ${req.url}`);
     next();
 });
 
-app.use("/api", rutasProductos)
-
 app.use((req, res, next) => {
     res.status(404).send('Recurso no encontrado o ruta inválida');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+// ***********************************************
+// ESTO REEMPLAZA AL APP.LISTEN()
+// ***********************************************
+
+// Si usas require (CommonJS)
+// module.exports = app; 
+
+// Si usas import (Módulos ES6) <-- Este es tu caso
+export default app;
